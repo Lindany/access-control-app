344962854ba93cda8c6e7c4e4ab4edbb4db4a469
Lindany <mabasolindaniricardo@gmail.com>
2020-06-11 17:38:07 +0200

    add all

---
+++ precache-manifest.e6fda121292604073717f4d178178b60.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95b39eb45a255e482cba1c0dd672818a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5b83875fe1417740b3af",
+    "url": "/static/css/2.828d3364.chunk.css"
+  },
+  {
+    "revision": "8e3d88a7a09de1208d16",
+    "url": "/static/css/main.9fb96a38.chunk.css"
+  },
+  {
+    "revision": "5b83875fe1417740b3af",
+    "url": "/static/js/2.18f2a0f1.chunk.js"
+  },
+  {
+    "revision": "8e3d88a7a09de1208d16",
+    "url": "/static/js/main.df16c858.chunk.js"
+  },
+  {
+    "revision": "562ff75797d7fbe3c03b",
+    "url": "/static/js/runtime~main.00f850e5.js"
+  }
+]);
